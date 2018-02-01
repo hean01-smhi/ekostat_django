b5900364028cf4e3e00fd8d92b21b888ea415303
Andreas Loo <andreas@gaup.se>
2018-02-01 18:15:19 +0100

    Use normalize.css

--- ekostat/ui/index.js
+++ ekostat/ui/index.js
@@ -2,6 +2,7 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './views/app';
+import 'normalize.css';
 import './index.css';
 
 ReactDOM.render(
