65244ae40f313f752e6e85277a106e6af3f6dec6
Andreas Loo <andreas@gaup.se>
2018-02-21 09:25:07 +0100

    Add calculator as submodule

--- lib/ekostat_calc/core/__init__.py
+++
@@ -1,8 +0,0 @@
-
-# import os, sys
-# currentdir = os.path.dirname(__file__)
-# sys.path.append(currentdir)
-
-# from core.test_module import test_print
-from .test_module import test_print
-
--- lib/ekostat_calc/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-
-# import os, sys
-# currentdir = os.path.dirname(__file__)
-# sys.path.append(currentdir)
-# 
-# from decorators import singleton
-
-from .decorators import singleton
