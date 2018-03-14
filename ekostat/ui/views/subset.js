9dbe0686341a0612dc4a3f44c5f259ad75dd006a
Andreas Loo <andreas@gaup.se>
2018-03-14 21:29:17 +0100

    Separate unit/period and quality/supporting elements into two steps in the UI

--- ekostat/ui/views/subset.js
+++ ekostat/ui/views/subset.js
@@ -53,174 +53,218 @@ const Subset = withRouter(({active, name, history, onClickEdit}) => {
 	);
 });
 
-const EditableSubset = () => (
-	<div className="editable-subset">
-		<div className="subset-selections">
-			<h3>Units</h3>
-			<div>
-				<label>
-					Water district
-					<select>
-						<option>Bottenhavet</option>
-						<option>Skagerakk</option>
-					</select>
-				</label>
-				<label>
-					Water bodies
-					<select>
-						<option>WB 1</option>
-						<option>WB 2</option>
-						<option>WB 3</option>
-					</select>
-				</label>
-				<label>
-					Period
-					<select>
-						<option>2007-2012</option>
-					</select>
-				</label>
-			</div>
-		</div>
-		<div className="subset-selections">
-			<h3>Biological quality elements</h3>
-			<fieldset>
-				<legend>Phytoplankton</legend>
-				<ul>
-					<li>
-						<label>
-							<input type="checkbox" />
-							Chlorophyll - default
-						</label>
-					</li>
-					<li>
-						<label>
-							<input type="checkbox" />
-							Biovolume - default
-						</label>
-					</li>
-					<li>
-						<label>
-							<input type="checkbox" />
-							Chlorophyll *- conc
-						</label>
-					</li>
-					<li>
-						<label>
-							<input type="checkbox" />
-							Chlorophyll *- satellite
-						</label>
-					</li>
-					<li>
-						<label>
-							<input type="checkbox" />
-							Chlorophyll *- model
-						</label>
-					</li>
-				</ul>
-			</fieldset>
-			<fieldset>
-				<legend>Benthic fauna</legend>
-				<ul>
-					<li>
-						<label>
-							<input type="checkbox" />
-							BQI - default
-						</label>
-					</li>
-					<li>
-						<label>
-							<input type="checkbox" />
-							BQI *- default
-						</label>
-					</li>
-					<li>
-						<label>
-							<input type="checkbox" />
-							BQI *- extrapolation
-						</label>
-					</li>
-				</ul>
-			</fieldset>
-			<fieldset>
-				<legend>Vegetation</legend>
-				<ul>
-					<li>
-						<label>
-							<input type="checkbox" />
-							MSMDI - default
-						</label>
-					</li>
-					<li>
-						<label>
-							<input type="checkbox" />
-							Eelgrass *- depthlimit
-						</label>
-					</li>
-					<li>
-						<label>
-							<input type="checkbox" />
-							Filamentoues *- %
-						</label>
-					</li>
-				</ul>
-			</fieldset>
-		</div>
-		<div className="subset-selections">
-			<h3>Supporting element</h3>
-			<fieldset>
-				<legend>Nutrients</legend>
-				<ul>
-					<li>
-						<label>
-							<input type="checkbox" />
-							N, winter - default
-						</label>
-					</li>
-					<li>
-						<label>
-							<input type="checkbox" />
-							N, summer - default
-						</label>
-					</li>
-					<li>
-						<label>
-							<input type="checkbox" />
-							P, winter - default
-						</label>
-					</li>
-					<li>
-						<label>
-							<input type="checkbox" />
-							P, summer - default
-						</label>
-					</li>
-				</ul>
-			</fieldset>
-			<fieldset>
-				<legend>Secchi</legend>
-				<ul>
-					<li>
-						<label>
-							<input type="checkbox" />
-							Secchi - default
-						</label>
-					</li>
-				</ul>
-			</fieldset>
-			<fieldset>
-				<legend>Oxygen</legend>
-				<ul>
-					<li>
-						<label>
-							<input type="checkbox" />
-							Oxygen 1 - default
-						</label>
-					</li>
-				</ul>
-			</fieldset>
-		</div>
-	</div>
-);
+class EditableSubset extends React.Component {
+
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			step: "1"
+		}
+
+		this.chooseStep = this.chooseStep.bind(this);
+	}
+
+	chooseStep(e) {
+		this.setState({step: e.target.value})
+	}
+
+	render() {
+		if (this.state.step == '1') {
+	 		return (
+				<div className="editable-subset">
+					<div className="subset-selections">
+						<h3>Units</h3>
+						<div>
+							<label>
+								Water district
+								<select>
+									<option>Bottenhavet</option>
+									<option>Skagerakk</option>
+								</select>
+							</label>
+							<label>
+								Water bodies
+								<select>
+									<option>WB 1</option>
+									<option>WB 2</option>
+									<option>WB 3</option>
+								</select>
+							</label>
+						</div>
+					</div>
+					<div className="subset-selections">
+						<h3>Period</h3>
+						<div>
+							<label>
+								From (year)
+								<input type="number" min="2001" max="2018" value="2007" />
+							</label>
+							<label>
+								To (year)
+								<input type="number" min="2001" max="2018" value="2011" />
+							</label>
+						</div>
+					</div>
+					<div className="actions">
+						<button onClick={this.chooseStep} value="2">
+							Show quality and supporting elements &raquo;
+						</button>
+					</div>
+				</div>
+			);
+	 	}
+	 	else {
+			return (
+				<div className="editable-subset">
+					<div className="subset-selections">
+						<h3>Biological quality elements</h3>
+						<fieldset>
+							<legend>Phytoplankton</legend>
+							<ul>
+								<li>
+									<label>
+										<input type="checkbox" />
+										Chlorophyll - default
+									</label>
+								</li>
+								<li>
+									<label>
+										<input type="checkbox" />
+										Biovolume - default
+									</label>
+								</li>
+								<li>
+									<label>
+										<input type="checkbox" />
+										Chlorophyll *- conc
+									</label>
+								</li>
+								<li>
+									<label>
+										<input type="checkbox" />
+										Chlorophyll *- satellite
+									</label>
+								</li>
+								<li>
+									<label>
+										<input type="checkbox" />
+										Chlorophyll *- model
+									</label>
+								</li>
+							</ul>
+						</fieldset>
+						<fieldset>
+							<legend>Benthic fauna</legend>
+							<ul>
+								<li>
+									<label>
+										<input type="checkbox" />
+										BQI - default
+									</label>
+								</li>
+								<li>
+									<label>
+										<input type="checkbox" />
+										BQI *- default
+									</label>
+								</li>
+								<li>
+									<label>
+										<input type="checkbox" />
+										BQI *- extrapolation
+									</label>
+								</li>
+							</ul>
+						</fieldset>
+						<fieldset>
+							<legend>Vegetation</legend>
+							<ul>
+								<li>
+									<label>
+										<input type="checkbox" />
+										MSMDI - default
+									</label>
+								</li>
+								<li>
+									<label>
+										<input type="checkbox" />
+										Eelgrass *- depthlimit
+									</label>
+								</li>
+								<li>
+									<label>
+										<input type="checkbox" />
+										Filamentoues *- %
+									</label>
+								</li>
+							</ul>
+						</fieldset>
+					</div>
+					<div className="subset-selections">
+						<h3>Supporting element</h3>
+						<fieldset>
+							<legend>Nutrients</legend>
+							<ul>
+								<li>
+									<label>
+										<input type="checkbox" />
+										N, winter - default
+									</label>
+								</li>
+								<li>
+									<label>
+										<input type="checkbox" />
+										N, summer - default
+									</label>
+								</li>
+								<li>
+									<label>
+										<input type="checkbox" />
+										P, winter - default
+									</label>
+								</li>
+								<li>
+									<label>
+										<input type="checkbox" />
+										P, summer - default
+									</label>
+								</li>
+							</ul>
+						</fieldset>
+						<fieldset>
+							<legend>Secchi</legend>
+							<ul>
+								<li>
+									<label>
+										<input type="checkbox" />
+										Secchi - default
+									</label>
+								</li>
+							</ul>
+						</fieldset>
+						<fieldset>
+							<legend>Oxygen</legend>
+							<ul>
+								<li>
+									<label>
+										<input type="checkbox" />
+										Oxygen 1 - default
+									</label>
+								</li>
+							</ul>
+						</fieldset>
+					</div>
+					<div className="actions">
+						<button onClick={this.chooseStep} value="1">
+							&laquo; Choose unit and period
+						</button>
+					</div>
+				</div>
+			);
+		}
+	}
+}
 
 
 export {EditableSubset, Subset}
