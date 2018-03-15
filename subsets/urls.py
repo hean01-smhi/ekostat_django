2f450c01447b12c4a7c1a2bcc950a105d3ce9297
Andreas Loo <andreas@gaup.se>
2018-03-15 11:57:41 +0100

    First draft Django views for subset listing using the calculator

--- subsets/urls.py
+++ subsets/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('<workspace>', views.index),
+	path('<workspace_uuid>', views.index),
 	path('<workspace>/edit', views.edit)
 ]
--- subsets/views.py
+++ subsets/views.py
@@ -3,110 +3,21 @@ import json
 from django.http import JsonResponse
 from django.views.decorators.http import require_GET, require_POST
 
-@require_GET
-def index(request, workspace):
-	if workspace != "my_workspace_1":
-		return JsonResponse({"error_message": "Could not find workspace"},status=404)
+import os
 
-	return JsonResponse({
-		"workspace": {
-			"alias": "My Workspace 1",
-			"name": workspace,
-		},
-		"subsets": [
-			{
-				"active": True,
-				"alias": "My Subset 1",
-				"name": "my_subset_1",
-				"water_districts": [
-					{
-						"label": "Bottenhavet",
-						"value": "Bottenhavet",
-						"selectable": True,
-						"selected": True
-					},
-					{
-						"label": "Skagerakk",
-						"value": "Skagerakk",
-						"selectable": True,
-						"selected": False,
-					}
-				],
-				"water_bodies": [
-					{
-						"label": "WB 1",
-						"value": "WB 1",
-						"selectable": True,
-						"selected": True,
-					},
-					{
-						"label": "WB 2",
-						"value": "WB 2",
-						"selectable": True,
-						"selected": True,
-					},
-					{
-						"label": "WB 3",
-						"value": "WB 3",
-						"selectable": True,
-						"selected": False,
-					},
-					{
-						"label": "WB 4",
-						"value": "WB 4",
-						"selectable": True,
-						"selected": True,
-					},
-				],
-				"periods": [
-					{
-						"label": "2006-2012",
-						"value": "2006-2012",
-						"selectable": True,
-						"selected": True,
-					},
-					{
-						"label": "2012-2017",
-						"value": "2012-2017",
-						"selectable": True,
-						"selected": False,
-					}
-				],
-				"quality_elements": [
-					{
-						"label": "Phytoplankton",
-						"children": [
-							{
-								"label": "Chlorophyll - default",
-								"value": "Chlorophyll - default",
-								"selectable": True,
-								"selected": True,
-							},
-							{
-								"label": "Biovolume - default",
-								"value": "Biovolume - default",
-								"selectable": True,
-								"selected": True,
-							}
-						]
-					}
-				],
-				"supporting_elements": [
-					{
-						"label": "Secchi",
-						"children": [
-							{
-								"label": "Secchi - default",
-								"value": "Secchi - default",
-								"selectable": True,
-								"selected": False
-							}
-						]
-					}
-				]
-			}
-		]
-	})
+from lib.ekostat_calculator.event_handler import EventHandler
+
+root_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'lib', 'ekostat_calculator')
+ekos = EventHandler(root_dir)
+
+user_id = 1
+
+@require_GET
+def index(request, workspace_uuid):
+	try:
+		return JsonResponse(ekos.request_subset_list({"user_id": user_id, "workspace_uuid": workspace_uuid}))
+	except Exception as e:
+		return JsonResponse({"error_message": "Could not found data."},status=404)
 
 @require_POST
 def edit(request, workspace):
