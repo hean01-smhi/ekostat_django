44a917fa610a8cd56613e98ac74e422c98231fcb
Andreas Loo <andreas@gaup.se>
2018-06-01 00:11:51 +0200

    Update user validation rules

--- accounts/validators.py
+++ accounts/validators.py
@@ -1,6 +1,7 @@
 import re
 
 from django.core import validators
+from django.core.exceptions import ValidationError
 from django.utils.deconstruct import deconstructible
 from django.utils.translation import gettext_lazy as _
 
@@ -11,3 +12,18 @@ class UsernameValidator(validators.RegexValidator):
         'Enter a valid username consisting of letters, '
         'numbers, underscores, or hyphens.'
     )
+    message_disallowed = _(
+        "Username '%(username)s' is not allowed."
+    )
+    disallowed = ['default', 'guest']
+
+    def __call__(self, value):
+        # Check if username is allowed
+        if value.lower() in self.disallowed:
+            raise ValidationError(
+                self.message_disallowed,
+                code=self.code,
+                params={'username': value}
+            )
+        # Check if username match regex
+        super().__call__(value)
